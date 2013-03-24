e54614f518f9f1519fac42f1c948942046353e3f
robinzoft <robinzoft@gmail.com>
2013-03-24 16:25:50 -0500

    implementando permisos en consulta_datos

--- consulta_datos.php
+++ consulta_datos.php
@@ -1,62 +1,57 @@
 <?php
 
 session_start();
-if (!isset($_SESSION['s_username'])) {
-	header('Location: index.php');
-	exit();
-} else {
-	if (isset($_POST['peticion'])) {
-		include_once 'conexion/pgsql.php';
-		$conexion = new ConexionPGSQL();
-		$conexion->conectar();
-		$peticion = $_POST['peticion'];
-		if ($peticion == "institucion") {
-			$resultado = $conexion->consulta("select idinstitucion, Siglas from tinstitucion");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				$jsondata = array();
-				for ($cont = 0; $cont < $filas; $cont++) {
-					$jsondata[$cont]['idinstitucion'] = pg_result($resultado, $cont, 0);
-					$jsondata[$cont]['siglas'] = pg_result($resultado, $cont, 1);
-				}
-			}
-		}
-		if ($peticion == "cargo") {
-			$resultado = $conexion->consulta("select idprivilegios, nombprivi from tprivilegios");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				$jsondata = array();
-				for ($cont = 0; $cont < $filas; $cont++) {
-					$jsondata[$cont]['idprivilegios'] = pg_result($resultado, $cont, 0);
-					$jsondata[$cont]['nombprivi'] = pg_result($resultado, $cont, 1);
-				}
-			}
-		}
-
-		if ($peticion == "unidad_medida") {
-			$resultado = $conexion->consulta("select idunidadmedida, abreviatura from tunidadmedida");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				$jsondata = array();
-				for ($cont = 0; $cont < $filas; $cont++) {
-					$jsondata[$cont]['idunidadmedida'] = pg_result($resultado, $cont, 0);
-					$jsondata[$cont]['abreviatura'] = pg_result($resultado, $cont, 1);
-				}
-			}
-		}
-
-		if ($peticion == "minimo_maximo") {
-			$idindicador = $_POST['indicador'];
-			$resultado = $conexion->consulta("select valorminimo, valormaximo from tindicador where idindicador='$idindicador'");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				for ($cont = 0; $cont < $filas; $cont++) {
-					$jsondata['minimo'] = pg_result($resultado, $cont, 0);
-					$jsondata['maximo'] = pg_result($resultado, $cont, 1);
-				}
+if (isset($_POST['peticion'])) {
+	include_once 'conexion/pgsql.php';
+	$conexion = new ConexionPGSQL();
+	$conexion->conectar();
+	$peticion = $_POST['peticion'];
+	if ($peticion == "institucion") {
+		$resultado = $conexion->consulta("select idinstitucion, Siglas from tinstitucion");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			$jsondata = array();
+			for ($cont = 0; $cont < $filas; $cont++) {
+				$jsondata[$cont]['idinstitucion'] = pg_result($resultado, $cont, 0);
+				$jsondata[$cont]['siglas'] = pg_result($resultado, $cont, 1);
 			}
 		}
 	}
-	echo json_encode($jsondata);
+	if ($peticion == "cargo") {
+		$resultado = $conexion->consulta("select idprivilegios, nombprivi from tprivilegios");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			$jsondata = array();
+			for ($cont = 0; $cont < $filas; $cont++) {
+				$jsondata[$cont]['idprivilegios'] = pg_result($resultado, $cont, 0);
+				$jsondata[$cont]['nombprivi'] = pg_result($resultado, $cont, 1);
+			}
+		}
+	}
+
+	if ($peticion == "unidad_medida") {
+		$resultado = $conexion->consulta("select idunidadmedida, abreviatura from tunidadmedida");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			$jsondata = array();
+			for ($cont = 0; $cont < $filas; $cont++) {
+				$jsondata[$cont]['idunidadmedida'] = pg_result($resultado, $cont, 0);
+				$jsondata[$cont]['abreviatura'] = pg_result($resultado, $cont, 1);
+			}
+		}
+	}
+
+	if ($peticion == "minimo_maximo") {
+		$idindicador = $_POST['indicador'];
+		$resultado = $conexion->consulta("select valorminimo, valormaximo from tindicador where idindicador='$idindicador'");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			for ($cont = 0; $cont < $filas; $cont++) {
+				$jsondata['minimo'] = pg_result($resultado, $cont, 0);
+				$jsondata['maximo'] = pg_result($resultado, $cont, 1);
+			}
+		}
+	}
 }
+echo json_encode($jsondata);
 ?>
--- consulta_datos_html.php
+++ consulta_datos_html.php
@@ -1,56 +1,51 @@
 <?php
 
 session_start();
-if (!isset($_SESSION['s_username'])) {
-	header('Location: index.php');
-	exit();
-} else {
-	if (isset($_POST['peticion'])) {
-		include_once 'conexion/pgsql.php';
-		$conexion = new ConexionPGSQL();
-		$conexion->conectar();
-		$peticion = $_POST['peticion'];
-		if ($peticion == "provincia") {
-			$resultado = $conexion->consulta("select * from tprovincia");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				echo '<option style="color: blue" disabled selected>Elija una Provincia</option>';
-				for ($cont = 0; $cont < $filas; $cont++) {
-					$codProvincia = pg_result($resultado, $cont, 0);
-					$nombre = pg_result($resultado, $cont, 1);
-					echo "<option value='" . $codProvincia . "'>" . $nombre . "</option>";
-				}
+if (isset($_POST['peticion'])) {
+	include_once 'conexion/pgsql.php';
+	$conexion = new ConexionPGSQL();
+	$conexion->conectar();
+	$peticion = $_POST['peticion'];
+	if ($peticion == "provincia") {
+		$resultado = $conexion->consulta("select * from tprovincia");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			echo '<option style="color: blue" disabled selected>Elija una Provincia</option>';
+			for ($cont = 0; $cont < $filas; $cont++) {
+				$codProvincia = pg_result($resultado, $cont, 0);
+				$nombre = pg_result($resultado, $cont, 1);
+				echo "<option value='" . $codProvincia . "'>" . $nombre . "</option>";
 			}
 		}
-		if ($peticion == "distrito") {
-			$codProvincia = $_POST['codProvincia'];
-			$resultado = $conexion->consulta("select ubigeo, nombre from tdistrito where codProvincia = '$codProvincia'");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				for ($cont = 0; $cont < $filas; $cont++) {
-					echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
-				}
+	}
+	if ($peticion == "distrito") {
+		$codProvincia = $_POST['codProvincia'];
+		$resultado = $conexion->consulta("select ubigeo, nombre from tdistrito where codProvincia = '$codProvincia'");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			for ($cont = 0; $cont < $filas; $cont++) {
+				echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
 			}
 		}
-		if ($peticion == "indicador") {
-			$resultado = $conexion->consulta("select idindicador, descripcion from tindicador where idinstitucion='" . $_SESSION["s_idinstitucion"] . "'");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				for ($cont = 0; $cont < $filas; $cont++) {
-					echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
-				}
+	}
+	if ($peticion == "indicador") {
+		$resultado = $conexion->consulta("select idindicador, descripcion from tindicador where idinstitucion='" . $_SESSION["s_idinstitucion"] . "'");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			for ($cont = 0; $cont < $filas; $cont++) {
+				echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
 			}
 		}
+	}
 
-		if ($peticion == "indicador_institucion") {
-			$idinstitucion = $_POST['institucion'];
-			$resultado = $conexion->consulta("select idindicador, descripcion from tindicador where idinstitucion='" . $idinstitucion . "'");
-			$filas = pg_numrows($resultado);
-			if ($filas != 0) {
-				echo '<option style="color: blue" disabled selected>Elija un Indicador</option>';
-				for ($cont = 0; $cont < $filas; $cont++) {
-					echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
-				}
+	if ($peticion == "indicador_institucion") {
+		$idinstitucion = $_POST['institucion'];
+		$resultado = $conexion->consulta("select idindicador, descripcion from tindicador where idinstitucion='" . $idinstitucion . "'");
+		$filas = pg_numrows($resultado);
+		if ($filas != 0) {
+			echo '<option style="color: blue" disabled selected>Elija un Indicador</option>';
+			for ($cont = 0; $cont < $filas; $cont++) {
+				echo "<option value='" . pg_result($resultado, $cont, 0) . "'>" . pg_result($resultado, $cont, 1) . "</option>";
 			}
 		}
 	}
